8f342a19a409113469a709e6344bfc63029f34d8
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2021-07-18 17:22:56 +0200

    Working on simple 1st person controller

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    float mouseX;
+
+    void Start()
+    {
+        mouseX=0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis
+    }
+}
