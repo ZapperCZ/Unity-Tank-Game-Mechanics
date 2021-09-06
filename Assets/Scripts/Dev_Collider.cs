395f9ad609fa12389a065431f83c9e347db9e3ba
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2021-09-06 14:56:43 +0200

    Working on collider view

--- Assets/Scripts/Dev_Collider.cs
+++ Assets/Scripts/Dev_Collider.cs
@@ -10,9 +10,10 @@ public class Dev_Collider : MonoBehaviour
     public bool isActive = false;
 
     public GameObject[] IgnoredParents;
+    public GameObject ColliderMeshParent;
 
-    List<GameObject> SceneObjectsWithColliders;
-    List<GameObject> SceneObjects;
+    List<GameObject> SceneObjectsWithCollider;
+    List<GameObject> SceneObjectsWithRenderer;
     Collider[] Colliders = {new BoxCollider(), new SphereCollider(), new CapsuleCollider(), new MeshCollider()};
 
     //TODO: Handle collider view when new objects are created
@@ -22,33 +23,62 @@ public class Dev_Collider : MonoBehaviour
         if (devMode)
         {
             Debug.Log("Colliders - Started Loading");
-            SceneObjectsWithColliders = new List<GameObject>();
-            SceneObjects = new List<GameObject>();
+            SceneObjectsWithCollider = new List<GameObject>();
+            SceneObjectsWithRenderer = new List<GameObject>();
+
+            //Get all objects in the scene and assign them to the appropiate list depending on the component
 
             foreach(GameObject sObject in UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects())
             {
-                //Add object to the list if it has a collider
                 if (!IgnoredParents.Contains(sObject))
                 {
-                    /*
-                    Debug.Log("----Parent----");
-                    Debug.Log(sObject.name);
-                    Debug.Log("----Children----");
-                    */
                     foreach (Transform child in sObject.GetComponentsInChildren<Transform>(true))
                     {
-                        SceneObjects.Add(child.gameObject);
                         //Debug.Log(child.name + " - " + HasComponent<Collider>(child.gameObject));
                         if (HasComponent<Collider>(child.gameObject))
                         {
-                            Debug.Log(child.GetComponent<Collider>().GetType());
-                            SceneObjectsWithColliders.Add(child.gameObject);
+                            //Debug.Log(child.GetComponent<Collider>().GetType());
+                            SceneObjectsWithCollider.Add(child.gameObject);
+                        }
+                        if (HasComponent<MeshRenderer>(child.gameObject))
+                        {
+                            SceneObjectsWithRenderer.Add(child.gameObject);
                         }
                     }
                 }
             }
             Debug.Log("Colliders - Finished Loading");
             Debug.Log("Colliders - Started Assigning Models");
+            foreach (GameObject colliderParent in SceneObjectsWithCollider)
+            {
+                GameObject newVisibleCollider = new GameObject();
+                bool isValid = true;
+                switch (Array.FindIndex(Colliders, c => c.GetType() == colliderParent.GetComponent<Collider>().GetType()))
+                {
+                    case 0:             //BoxCollider
+                        newVisibleCollider = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        break;
+                    case 1:             //SphereCollider
+                        newVisibleCollider = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                        break;
+                    case 2:             //CapsuleCollider
+                        newVisibleCollider = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                        break;
+                    case 3:             //MeshCollider
+                        newVisibleCollider.AddComponent<MeshRenderer>();
+                        break;
+                    default:
+                        Debug.Log("Colliders - Unrecognized collider in: "+colliderParent.name);
+                        isValid = false;
+                        break;
+                }
+                if (isValid)
+                {
+                    Destroy(newVisibleCollider.GetComponent<Collider>());
+                    newVisibleCollider.transform.SetParent(ColliderMeshParent.transform);
+                }
+            }
+            Debug.Log("Colliders - Finished Assigning Models");
         }
     }
 
@@ -64,7 +94,12 @@ public class Dev_Collider : MonoBehaviour
     }
     void SwitchViewModels(bool targetMode)  //0 = normal, 1 = colliders
     {
-
+        Debug.Log("Collider view - " + targetMode);
+        //Disables all mesh renderers in scene
+        foreach(GameObject gObj in SceneObjectsWithRenderer)
+        {
+            gObj.GetComponent<MeshRenderer>().enabled = !targetMode;
+        }
     }
     bool HasComponent <T>(GameObject inputObject) where T:Component
     {
