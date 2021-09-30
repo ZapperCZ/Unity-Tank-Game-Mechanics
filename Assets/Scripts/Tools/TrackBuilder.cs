a79526a19289f686a32ca9128b58a3f1038f7e21
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2021-09-30 12:30:59 +0200

    Track Builder - Created basic foundation
    Created a script that runs in editor mode and gets disabled when the object it is assigned to doesn't have required components.

---
+++ Assets/Scripts/Tools/TrackBuilder.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TrackBuilder : MonoBehaviour
+{
+    [SerializeField]
+    enum TrackType
+    {
+        Flat,
+        Loop
+    }
+    [Range(5,100)]
+    [SerializeField] float trackLength = 10f;
+    [Range(0.05f,1f)]
+    [SerializeField] float linkSpacing;
+
+    GameObject TrackLink;
+
+    private void OnEnable()
+    {
+        TrackLink = this.gameObject;    //Uses the object it is assigned to as a track link
+        if(!HasComponent<MeshRenderer>(TrackLink) || !HasComponent<Collider>(TrackLink) || !HasComponent<Rigidbody>(TrackLink))
+        {
+            Debug.LogError("Track Builder - " + TrackLink.name + " - Required components not found, disabling script");
+            this.enabled = false;
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+    bool HasComponent<T>(GameObject inputObject) where T : Component //Returns whether the input object has a collider or not
+    {
+        return inputObject.GetComponent<T>() != null;
+    }
+}
