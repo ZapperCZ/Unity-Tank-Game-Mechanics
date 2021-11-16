c302cefd6d31ab366dfaf0771f0e1d079a88a1ab
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2021-11-16 15:12:58 +0100

    Improved spin script, added direction flipping and changing angular velocity limit

--- Assets/Scripts/Tank/Suspension/Spin.cs
+++ Assets/Scripts/Tank/Suspension/Spin.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    public float Torque = 100f;
+    public float Torque = 0f;
+    public bool FlipSpinDirection = false;
+    public float MaxAngularVelocity = 7;
+    int Direction = 1;
+
+    private void OnValidate()
+    {
+        Direction = FlipSpinDirection ? -1 : 1;                 //If enabled, direction is flipped (-1), else it is unchanged (1)
+        MaxAngularVelocity = MaxAngularVelocity < 0 ? 0 : MaxAngularVelocity;   //If less than 0, change to 0, otherwise leave as is
+    }
     private void FixedUpdate()
     {
-        this.GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0,Torque,0));
+        this.GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0,Torque * Direction,0));
+        this.GetComponent<Rigidbody>().maxAngularVelocity = MaxAngularVelocity;
     }
 }
