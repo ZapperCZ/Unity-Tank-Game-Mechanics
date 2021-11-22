4f757eee0731910ca573675be605b369d77f72a3
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2021-11-22 11:17:24 +0100

    Created a method to control wheel connectivity
    Method takes 2 wheels as input and based on their type outputs whether they can be connected or not

--- Assets/Scripts/Tools/TrackWrapper.cs
+++ Assets/Scripts/Tools/TrackWrapper.cs
@@ -40,9 +40,7 @@ public class TrackWrapper : MonoBehaviour
                     NeighbouringWheels[i, 1] = Sprocket;
                 }
             }
-
-
-            //Debug.Log(@$"Track Wrapper - {transform.name} - {Wheels[i].name}: {NeighbouringWheels[i,0].name}, {NeighbouringWheels[i,1].name}");
+            Debug.Log(@$"Track Wrapper - {transform.name} - {Wheels[i].name}: {NeighbouringWheels[i,0].name}, {NeighbouringWheels[i,1].name}");
         }
 
     }
@@ -74,24 +72,25 @@ public class TrackWrapper : MonoBehaviour
         //TODO: Create a method that takes 2 wheels as an input and outputs whether the wheels can be connected or not
         //TODO: Move the special condition for sprocket and idler into here
 
-        float minDist = float.MaxValue;
-        float minDist2 = float.MaxValue;
+        float minDist = float.MaxValue;         //Distance of the closest wheel
+        float minDist2 = float.MaxValue;        //Distance of the second closest wheel
         float tempDist;
         Vector3 wheelPos = referenceWheel.position;
-
+            
         foreach(Transform Wheel in Wheels)
         {
-            //Since there are no wheels to compare distance to on the first iteration, the first found wheel is the closest one
+            //Runs on the first iteration
             if (ClosestWheels[0] == null) 
             {
+                //Since the new wheel is the first one found, it is also the closest one
                 ClosestWheels[0] = Wheel;
-                minDist = Vector3.Distance(referenceWheel.position,Wheel.position);     //Distance between the closest wheel and the input wheel
+                minDist = Vector3.Distance(wheelPos, Wheel.position);     //Distance between the closest wheel and the input wheel
                 continue;
             }
             //Runs on the second iteration
             if (ClosestWheels[1] == null)
             {
-                tempDist = Vector3.Distance(referenceWheel.position, Wheel.position);
+                tempDist = Vector3.Distance(wheelPos, Wheel.position);
                 if (tempDist < minDist)
                 {
                     //The new wheel is closer than the previous one
@@ -117,12 +116,38 @@ public class TrackWrapper : MonoBehaviour
     }
 
 
-    
+    bool AreWheelsConnectable(Wheel firstWheel, Wheel secondWheel)
+    {
+        bool result = false;
+        switch (firstWheel.WheelType)
+        {
+            //Ordered by the probability of the wheel being the corresponding type (highest to lowest)
+            case Wheel.TypeOfWheel.Roadwheel:
+                result = secondWheel.WheelType != Wheel.TypeOfWheel.ReturnRoller;
+                break;
+            case Wheel.TypeOfWheel.ReturnRoller:
+                result = secondWheel.WheelType != Wheel.TypeOfWheel.Roadwheel;
+                break;
+            case Wheel.TypeOfWheel.Sprocket:
+                //Doesn't handle the special case where there are no return rollers, this is handled elsewhere
+                result = secondWheel.WheelType != Wheel.TypeOfWheel.Idler && secondWheel.WheelType != Wheel.TypeOfWheel.Sprocket;
+                break;
+            case Wheel.TypeOfWheel.Idler:
+                //Doesn't handle the special case where there are no return rollers, this is handled elsewhere
+                result = secondWheel.WheelType != Wheel.TypeOfWheel.Sprocket && secondWheel.WheelType != Wheel.TypeOfWheel.Idler;
+                break;
+        }
+        return result;
+    }
 
     Line CreateOuterTangent(Circle circle_A, Circle circle_B, bool isUpper)     //isUpper determines whether the tangent should be made on the "upper" sides of the circles or on the "lower" sides
     {
         //TODO: Finish this
         Line tangent = new Line();
         return tangent;
+    }
+    bool HasComponent<T>(GameObject inputObject) where T : Component //Returns whether the input object has a component or not
+    {
+        return inputObject.GetComponent<T>() != null;
     }
 }
