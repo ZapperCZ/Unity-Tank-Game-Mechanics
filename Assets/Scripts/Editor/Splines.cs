6c5b36772351f7088e1c7c367922591629f27e89
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2021-12-06 00:50:38 +0100

    Following the guide
    Created a line that renders in the editor and can be transformed

--- Assets/Scripts/Editor/Splines.cs
+++ Assets/Scripts/Editor/Splines.cs
@@ -8,10 +8,13 @@ public class Splines : Editor
 	{
 		Line line = target as Line;
 		Transform handleTransform = line.transform;
+		Quaternion handleRotation = handleTransform.rotation;
 		Vector3 p0 = handleTransform.TransformPoint(line.p0);
 		Vector3 p1 = handleTransform.TransformPoint(line.p1);
 
 		Handles.color = Color.white;
 		Handles.DrawLine(p0, p1);
+		Handles.DoPositionHandle(p0, handleRotation);
+		Handles.DoPositionHandle(p1, handleRotation);
 	}	
 }
