66fd763344ad19ab97714014bb38624e5b5e1a10
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2021-12-06 11:42:39 +0100

    Upgraded the quadratic bezier curve to a cubic bezier curve

--- Assets/Scripts/Editor/BezierCurveInspector.cs
+++ Assets/Scripts/Editor/BezierCurveInspector.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 [CustomEditor(typeof(BezierCurve))]
 public class BezierCurveInspector : Editor
 {
-
 	private BezierCurve curve;
 	private Transform handleTransform;
 	private Quaternion handleRotation;
@@ -19,17 +18,22 @@ public class BezierCurveInspector : Editor
 		Vector3 p0 = ShowPoint(0);
 		Vector3 p1 = ShowPoint(1);
 		Vector3 p2 = ShowPoint(2);
+		Vector3 p3 = ShowPoint(3);
 
 		Handles.color = Color.gray;
 		Handles.DrawLine(p0, p1);
-		Handles.DrawLine(p1, p2);
+		Handles.DrawLine(p2, p3);
 
-		Handles.color = Color.white;
 		Vector3 lineStart = curve.GetPoint(0f);
+		Handles.color = Color.green;
+		Handles.DrawLine(lineStart, lineStart + curve.GetDirection(0f));
 		for (int i = 1; i <= lineSteps; i++)
 		{
 			Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
+			Handles.color = Color.white;
 			Handles.DrawLine(lineStart, lineEnd);
+			Handles.color = Color.green;
+			Handles.DrawLine(lineEnd, lineEnd + curve.GetDirection(i / (float)lineSteps));
 			lineStart = lineEnd;
 		}
 	}
