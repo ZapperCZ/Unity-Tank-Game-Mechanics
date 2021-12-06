b89f286174523e828896b18df2712321728bb9d1
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2021-12-06 12:09:32 +0100

    Changes that weren't in the previous commit for some reason

--- Assets/Scripts/Editor/BezierCurveInspector.cs
+++ Assets/Scripts/Editor/BezierCurveInspector.cs
@@ -8,9 +8,11 @@ public class BezierCurveInspector : Editor
 	private Transform handleTransform;
 	private Quaternion handleRotation;
 	private const int lineSteps = 10;
+	private const float directionScale = 0.5f;
 
 	private void OnSceneGUI()
 	{
+
 		curve = target as BezierCurve;
 		handleTransform = curve.transform;
 		handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
@@ -24,17 +26,18 @@ public class BezierCurveInspector : Editor
 		Handles.DrawLine(p0, p1);
 		Handles.DrawLine(p2, p3);
 
-		Vector3 lineStart = curve.GetPoint(0f);
+		ShowDirections();
+		Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+	}
+	private void ShowDirections()
+	{
 		Handles.color = Color.green;
-		Handles.DrawLine(lineStart, lineStart + curve.GetDirection(0f));
+		Vector3 point = curve.GetPoint(0f);
+		Handles.DrawLine(point, point + curve.GetDirection(0f) * directionScale);
 		for (int i = 1; i <= lineSteps; i++)
 		{
-			Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
-			Handles.color = Color.white;
-			Handles.DrawLine(lineStart, lineEnd);
-			Handles.color = Color.green;
-			Handles.DrawLine(lineEnd, lineEnd + curve.GetDirection(i / (float)lineSteps));
-			lineStart = lineEnd;
+			point = curve.GetPoint(i / (float)lineSteps);
+			Handles.DrawLine(point, point + curve.GetDirection(i / (float)lineSteps) * directionScale);
 		}
 	}
 
