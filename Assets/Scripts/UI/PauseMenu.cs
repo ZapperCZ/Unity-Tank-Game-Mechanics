b9538dbb8dc02fcf23ce98c9df4111c59e1d9af1
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2021-12-11 16:12:38 +0100

    Started working on a basic pause menu

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    [SerializeField] GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Pause Key"))
+        {
+            isPaused = !isPaused;
+            pauseMenuUI.SetActive(isPaused);
+            Time.timeScale = (float)Convert.ToInt32(!isPaused);
+        }
+    }
+
+}
