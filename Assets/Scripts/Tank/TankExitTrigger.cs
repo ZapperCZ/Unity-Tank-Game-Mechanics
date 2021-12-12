156b6ad8d77bb857d8718703d7e08818bde19b84
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2021-12-12 21:50:09 +0100

    Uncommitted changes

--- Assets/Scripts/Tank/TankExitTrigger.cs
+++ Assets/Scripts/Tank/TankExitTrigger.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class TankExitTrigger : MonoBehaviour
 {
-    TankController TankController;
+    [SerializeField] TankController TankController;
 
     void Start()
     {
