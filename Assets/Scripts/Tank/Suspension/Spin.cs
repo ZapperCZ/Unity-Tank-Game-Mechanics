2844d78a01b460697340ceb3dcdbda4a3a600830
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2021-12-12 23:38:25 +0100

    Basic controls of the tank are now functional

--- Assets/Scripts/Tank/Suspension/Spin.cs
+++ Assets/Scripts/Tank/Suspension/Spin.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    [SerializeField] bool applyTorque = false;          //Whether the torque should be applied or not
-    [SerializeField] float Torque = 0f;                 //The amount of torque applied
-    [SerializeField] bool FlipSpinDirection = false;    //Whether the spinning direction should be flipped or not
+    public bool applyTorque = false;          //Whether the torque should be applied or not
+    public float Torque = 0f;                 //The amount of torque applied
+    public bool FlipSpinDirection = false;    //Whether the spinning direction should be flipped or not
     [SerializeField] float MaxAngularVelocity = 20;     //The rotational limit, by default set to 7 by Unity
 
     int Direction = 1;
@@ -12,8 +12,11 @@ public class Spin : MonoBehaviour
 
     private void OnValidate()
     {
-        Direction = FlipSpinDirection ? -1 : 1;                 //If enabled, direction is flipped (-1), else it is unchanged (1)
         MaxAngularVelocity = MaxAngularVelocity < 0 ? 0 : MaxAngularVelocity;   //If less than 0, change to 0, otherwise leave as is
+    }
+    private void Update()
+    {
+        Direction = FlipSpinDirection ? -1 : 1;                 //If enabled, direction is flipped (-1), else it is unchanged (1)
     }
     private void FixedUpdate()
     {
--- Assets/Scripts/Tank/TankController.cs
+++ Assets/Scripts/Tank/TankController.cs
@@ -7,34 +7,104 @@ public class TankController : MonoBehaviour
     public GameObject LeftSprocket;
     public GameObject RightSprocket;
 
-    [HideInInspector] public Vector3 leftExitPos;
-    [HideInInspector] public Vector3 rightExitPos;
+    public float responsiveness = 0.05f;
+    [SerializeField] float maxTorque = 800f;
+    float currentLeftTorque = 0f;
+    float currentRightTorque = 0f;
+    bool leftReverse = false;
+    bool rightReverse = false;
+    public Vector3 leftExitPos;
+    public Vector3 rightExitPos;
+
+    Spin LeftSprocketSpin;
+    Spin RightSprocketSpin;
+    float torqueStep = 0f;
     bool isSpaceOnLeft = true;
     bool isSpaceOnRight = true;
 
     public Vector3 CameraOffset;
 
     [SerializeField] Transform Player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        LeftSprocketSpin.applyTorque = true;
+        RightSprocketSpin.applyTorque = true;
+    }
+    private void OnDisable()
+    {
+        LeftSprocketSpin.applyTorque = false;
+        RightSprocketSpin.applyTorque = false;
+    }
+    void Awake()
+    {
+        torqueStep = maxTorque / 100;
+        LeftSprocketSpin = LeftSprocket.GetComponent<Spin>();
+        RightSprocketSpin = RightSprocket.GetComponent<Spin>();
+    }
     void Update()
     {
         GetInput();
         if (Input.GetButtonDown("Use Key"))
         {
             Player.GetComponent<PlayerControllerManager>().SwitchController(true, this.isSpaceOnLeft, this.isSpaceOnRight);
-            Debug.Log("Cock");
         }
+        ApplyTorque();
     }
-
+    void ApplyTorque()
+    {
+        LeftSprocketSpin.FlipSpinDirection = leftReverse;
+        RightSprocketSpin.FlipSpinDirection = rightReverse;
+        LeftSprocketSpin.Torque = currentLeftTorque;
+        RightSprocketSpin.Torque = currentRightTorque;
+    }
     void GetInput()
     {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        if (vertical != 0)          //W or S is being pressed
+        {
+            if(vertical > 0)        //W is being pressed
+            {
+                if(currentLeftTorque < maxTorque)
+                    currentLeftTorque += torqueStep;
+                if (currentRightTorque < maxTorque)
+                    currentRightTorque += torqueStep;
 
+                leftReverse = false;
+                rightReverse = false;
+            }
+            if (vertical < 0)        //S is being pressed
+            {
+                if (currentLeftTorque < maxTorque)
+                    currentLeftTorque += torqueStep;
+                if (currentRightTorque < maxTorque)
+                    currentRightTorque += torqueStep;
+
+                leftReverse = true;
+                rightReverse = true;
+            }
+        }
+        else
+        {
+            if (currentLeftTorque > 0)
+                currentLeftTorque -= torqueStep;
+            if (currentRightTorque > 0)
+                currentRightTorque -= torqueStep;
+        }
+        if (horizontal != 0)        //A or D is being pressed
+        {
+            if (horizontal > 0)        //D is being pressed
+            {
+                leftReverse = false;
+                rightReverse = true;
+            }
+            if (horizontal < 0)        //A is being pressed
+            {
+                leftReverse = true;
+                rightReverse = false;
+            }
+        }
     }
     public void EvaluateTrigger(double localX, bool isExiting)
     {
