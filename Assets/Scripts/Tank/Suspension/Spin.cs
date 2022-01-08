84ed846ed0694996715c303db8c129a2c5e2f749
ZapperCZ <janfridl@seznam.cz>
2022-01-08 19:44:32 +0100

    Added an axis to the spin script

--- Assets/Scripts/Tank/Suspension/Spin.cs
+++ Assets/Scripts/Tank/Suspension/Spin.cs
@@ -5,6 +5,7 @@ public class Spin : MonoBehaviour
     public bool applyTorque = false;          //Whether the torque should be applied or not
     public float Torque = 0f;                 //The amount of torque applied
     public bool FlipSpinDirection = false;    //Whether the spinning direction should be flipped or not
+    [SerializeField] Vector3 SpinAxis = new Vector3(0, 1, 0);
     [SerializeField] float MaxAngularVelocity = 20;     //The rotational limit, by default set to 7 by Unity
 
     int Direction = 1;
@@ -12,7 +13,8 @@ public class Spin : MonoBehaviour
 
     private void OnValidate()
     {
-        MaxAngularVelocity = MaxAngularVelocity < 0 ? 0 : MaxAngularVelocity;   //If less than 0, change to 0, otherwise leave as is
+        MaxAngularVelocity = MaxAngularVelocity < 0 ? 0 : MaxAngularVelocity;       //If less than 0, change to 0, otherwise leave as is
+        this.GetComponent<Rigidbody>().maxAngularVelocity = MaxAngularVelocity;     //Apply the rotational velocity limit
     }
     private void Update()
     {
@@ -22,8 +24,7 @@ public class Spin : MonoBehaviour
     {
         if(applyTorque)
         {
-            this.GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(Torque * Direction, 0, 0));  //Apply the torque
+            this.GetComponent<Rigidbody>().AddRelativeTorque(SpinAxis * Torque * Direction);  //Apply the torque
         }
-        this.GetComponent<Rigidbody>().maxAngularVelocity = MaxAngularVelocity;                 //Apply the rotational velocity limit
     }
 }
