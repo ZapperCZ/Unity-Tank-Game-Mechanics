c897f4dff8cd7d72127201f492c6364dddf53f0f
ZapperCZ <janfridl@seznam.cz>
2022-01-09 18:08:51 +0100

    Added zoom to the vehicle camera controller

--- Assets/Scripts/Tank/VehicleCameraController.cs
+++ Assets/Scripts/Tank/VehicleCameraController.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class VehicleCameraController : MonoBehaviour
 {
     float x, y;
-    public float sensitivity, distance;
-    public Vector2 MinMax;
+    float scroll;
+    public float sensitivity, distance, minDistance, maxDistance, scrollSens;
+    public Vector2 MinMax;                      //The maximum and minimum angle
     public Transform FocusPoint;
 
     void Update()
     {
-        x += Input.GetAxis("Mouse Y") * sensitivity * -1;
-        y += Input.GetAxis("Mouse X") * sensitivity;
+        GetInput();
+
+        Debug.Log(scroll);
+
+        if(scroll < 0 && distance < maxDistance)            //Scrolling away (down)
+        {
+            distance += scrollSens;
+        }
+        else if (scroll > 0 && distance > minDistance)      //Scrolling in (up)
+        {
+            distance -= scrollSens;
+        }
 
         x = Mathf.Clamp(x, MinMax.x, MinMax.y);
 
         transform.eulerAngles = new Vector3(x, y + 180, 0);
-
         transform.position = FocusPoint.position - transform.forward * distance;
+    }
+    void GetInput()
+    {
+        x += Input.GetAxis("Mouse Y") * sensitivity * -1;
+        y += Input.GetAxis("Mouse X") * sensitivity;
+
+        scroll = Input.GetAxis("Mouse ScrollWheel");
     }
 }
