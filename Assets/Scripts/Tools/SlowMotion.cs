83782bb30aecf45f6501c82d98755d0755cc61b9
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2022-01-24 10:38:38 +0100

    Started working on a slow motion feature

---
+++ Assets/Scripts/Tools/SlowMotion.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SlowMotion : MonoBehaviour
+{
+    [SerializeField] int[] multiplierArray = new int[] {1,2,4,8};
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
