b4159170e795630bcd687bb5f16a910115677e06
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2022-01-25 13:02:10 +0100

    Finished slowmotion

--- Assets/Scripts/Tools/SlowMotion.cs
+++ Assets/Scripts/Tools/SlowMotion.cs
@@ -1,14 +1,18 @@
+using System;
 using UnityEngine;
 
 public class SlowMotion : MonoBehaviour
 {
     [SerializeField] int[] slowmoMultiplier = new int[] {1,2,4,8};
-    [SerializeField] int currentMultiplierIndex = 0;
+    [SerializeField] int currentMultiplierIndex = 1;
     [SerializeField] bool slowmoActive;
+    [SerializeField] float inputMsDelay = 200f;      //The delay on control input in ms    
 
     float slowmoHorizontalAxis = 0f;
-    float slowmoVerticalAxis = 0f;
     bool slowmoModifier = false;
+    bool increaseSlowmo = false;
+    bool decreaseSlowmo = false;
+    float internalTimer = 0f;
 
     // Update is called once per frame
     void OnValidate()
@@ -17,34 +21,55 @@ public class SlowMotion : MonoBehaviour
     }
     void Update()
     {
+        internalTimer = internalTimer + Time.deltaTime / Time.timeScale;        //A system that always increases the time uniformly, no matter the FPS or time scale
+
         GetInput();
 
-
+        if (slowmoActive)
+        {
+            Time.timeScale = 1f / slowmoMultiplier[currentMultiplierIndex];
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
     }
     void GetInput()
     {
         slowmoHorizontalAxis = Input.GetAxis("Slowmotion Horizontal Axis");
         slowmoModifier = Input.GetButton("Slowmotion Modifier");
 
-        if (Input.GetButtonDown("Slowmotion Toggle"))
-        {
-            slowmoActive = !slowmoActive;
-        }
         if (slowmoModifier)
         {
+            if (Input.GetButtonDown("Slowmotion Toggle"))
+            {
+                slowmoActive = !slowmoActive;
+            }
             if (slowmoActive && slowmoHorizontalAxis != 0)
             {
                 if (slowmoHorizontalAxis > 0)           //Player pressed left
                 {
-                    currentMultiplierIndex++;
+                    increaseSlowmo = true;
                 }
-                else
+                else                                    //Player pressed right
                 {
-                    currentMultiplierIndex--;           //Player pressed right
+                    decreaseSlowmo = true;
                 }
-                HandleIndexOverflow();
+                if (internalTimer > inputMsDelay / 1000)
+                {
+                    internalTimer = 0;
+                }
             }
         }
+        if (internalTimer > inputMsDelay / 1000)      //Only gets run once the internal timer has reached the input delay value
+        {
+            currentMultiplierIndex += Convert.ToInt32(increaseSlowmo);
+            currentMultiplierIndex -= Convert.ToInt32(decreaseSlowmo);
+            increaseSlowmo = false;
+            decreaseSlowmo = false;
+
+        }
+        HandleIndexOverflow();
     }
     void HandleIndexOverflow()
     {
