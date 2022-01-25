4aaae2530aaf7adc72000110167c684db5a4d260
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2022-01-25 14:12:12 +0100

    Minor code changes

--- Assets/Scripts/Tools/SlowMotion.cs
+++ Assets/Scripts/Tools/SlowMotion.cs
@@ -67,7 +67,6 @@ public class SlowMotion : MonoBehaviour
             currentMultiplierIndex -= Convert.ToInt32(decreaseSlowmo);
             increaseSlowmo = false;
             decreaseSlowmo = false;
-
         }
         HandleIndexOverflow();
     }
