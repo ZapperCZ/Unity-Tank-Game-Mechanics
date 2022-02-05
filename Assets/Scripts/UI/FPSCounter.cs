11c334a172f2724d56ddcc60bf15a48cc9eb084d
ZapperCZ <46494869+ZapperCZ@users.noreply.github.com>
2022-02-05 13:21:42 +0100

    Created a barely working FPS counter

---
+++ Assets/Scripts/UI/FPSCounter.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    [SerializeField] GameObject CounterUI;
+    [SerializeField] float updateInterval = 0.5f; //How often should the number update
+
+    float accum = 0.0f;
+    int frames = 0;
+    float timeleft;
+    float fps;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        timeleft = updateInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeleft -= Time.deltaTime;
+        accum += Time.timeScale / Time.deltaTime;
+        ++frames;
+
+        // Interval ended - update GUI text and start new interval
+        if (timeleft <= 0.0)
+        {
+            // display two fractional digits (f2 format)
+            fps = (accum / frames);
+            timeleft = updateInterval;
+            accum = 0.0f;
+            frames = 0;
+        }
+    }
+    void OnGUI()
+    {
+        CounterUI.GetComponent<UnityEngine.UI.Text>().text = Convert.ToInt32(fps).ToString();
+    }
+}
